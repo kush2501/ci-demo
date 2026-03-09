0ebee71854a2e7f1188690d2168c773f96206b7a
kush2501 <coder8342@gmail.com>
2026-03-09 12:28:10 +0530

    something usual

---
+++ .github/workflows/test_app.py
@@ -0,0 +1,15 @@
+import unittest
+from app import add, sub, mul
+class TestMathFunctions(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(4, 5), 9)
+        self.assertEqual(add(-1, 1), 0)
+    def test_sub(self):
+        self.assertEqual(sub(4, 5), -1)
+        self.assertEqual(sub(-1, -1), 0)
+    def test_mul(self):
+        self.assertEqual(mul(4, 5), 20)
+        self.assertEqual(mul(2, 3), 6)
+
+if __name__ == '__main__':
+    unittest.main()
