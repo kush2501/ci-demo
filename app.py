17078237c91844fe6a2c127ab128a2cfc891620a
kush2501 <coder8342@gmail.com>
2026-03-09 11:00:57 +0530

    fixed linting issues

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
 
+def sub(a, b): 
+    return a - b
 
-
-
-
-
-def sub(a,b): return a-b
-
-result1 = add(4,5)
-result2 = sub(5,6)
+result1 = add(4, 5)
+result2 = sub(5, 6)
 
 print(result1,result2)
